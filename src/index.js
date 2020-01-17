86e852f486afef2a1393b323e01deed97b3fc7f2
Mike Nguyen <mike.nguyen@L3237.fritz.box>
2020-01-18 09:27:41 +1300

    Base project structure setup for react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+        </Switch>
+    </Router>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'));
+serviceWorker.register();
