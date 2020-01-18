b989b0ebe19df0556523d3c4619c9d166d4d461d
Mike Nguyen <mike.nguyen0719@gmail.com>
2020-01-18 14:44:49 +1300

    Redux - add setUser action and reducer

--- src/index.js
+++ src/index.js
@@ -9,16 +9,20 @@ import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
 
 import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import {composeWithDevTool} from 'redux-devtools-extension';
+import {Provider, connect} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(() => {}, composeWithDevTool());
+import rootReducer from './reducers';
+import {setUser} from './actions'
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends Component {
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                this.props.setUser(user);
                 this.props.history.push("/");
             }
         });
@@ -35,7 +39,7 @@ class Root extends Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, {setUser})(Root));
 
 ReactDOM.render(
     <Provider store={store}>
