ad44c1fb2e613e4d61ac99b482707dd653723f0a
Mike Nguyen <mike.nguyen0719@gmail.com>
2020-01-18 15:37:19 +1300

    App - SideMenu Dropdown options

---
+++ src/components/SidePanel/UserPanel/UserPanel.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {Grid, Header, Icon, Dropdown} from 'semantic-ui-react';
+
+class UserPanel extends Component {
+    dropdownOptions = () => [
+        {
+            key: "user",
+            text: <span>Signed in as <strong>User</strong></span>, 
+            disabled: true
+        },
+        {
+            key: "avatar",
+            text: <span>Change Avatar</span>, 
+        },
+        {
+            key: "user",
+            text: <span>Sign Out</span>, 
+        },
+    ];
+
+    render() {
+        return(
+            <Grid style={{background: '#4c3c4c'}}>
+                <Grid.Column>
+                    <Grid.Row style={{padding: '1.2em', margin: 0}}>
+                        {
+                            <Header inverted floated="left" as="h2">
+                                <Icon name="code"/>
+                                <Header.Content>SlackChat</Header.Content>
+                            </Header>
+                        }
+                    </Grid.Row>
+                    {/* User dropdown */}
+                    <Header style={{padding: '0.25em'}} as="h4" inverted>
+                        <Dropdown trigger={<span>Users</span>} options={this.dropdownOptions()}/>
+                    </Header>
+                </Grid.Column>
+            </Grid>
+        );
+    }
+}
+
+export default UserPanel;
